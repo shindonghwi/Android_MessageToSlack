3fdb2ebf282aaf971dccd2e6e760a35bbc26f1c4
shindonghwi <ehdgnl8940@naver.com>
2022-08-19 11:21:50 +0900

    feat: sms 메세지 합치는 기능 추가

--- app/src/main/java/io/orot/messagetoslack/SMSReceiver.kt
+++ app/src/main/java/io/orot/messagetoslack/SMSReceiver.kt
@@ -15,65 +15,68 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import kotlin.math.log
 
 class SMSReceiver : BroadcastReceiver() {
 
     var compositeDisposable = CompositeDisposable()
+    val TAG = "onReceive"
 
     override fun onReceive(context: Context?, intent: Intent?) {
 
-        Log.d("ASdsadsdaads", "onReceive: ")
+        Log.d(TAG, "onReceive: ")
         if (!intent?.action.equals(Telephony.Sms.Intents.SMS_RECEIVED_ACTION)) return
-        Log.d("ASdsadsdaads", "called: ")
+        Log.d(TAG, "SMS called: ")
+
         val extractMessages = Telephony.Sms.Intents.getMessagesFromIntent(intent)
+        var totalMsg = ""
+        var phone = ""
+
         extractMessages.forEach { smsMessage ->
-            val TAG = "ASdkjl;ass"
-            Log.d(TAG, "DisplayMEssageBody :: " + smsMessage.getDisplayMessageBody());
+
+            Log.d(TAG, "DisplayMEssageBody :: " + smsMessage.displayMessageBody);
             Log.d(TAG, "DisplayOrinatingAddress" + smsMessage.displayOriginatingAddress);
-            Log.d(TAG, "EmailBody :: " + smsMessage.getEmailBody());
-            Log.d(TAG, "originatingAddress :: " + smsMessage.getOriginatingAddress());
-            Log.d(TAG, "MessageBody :: " + smsMessage.getMessageBody());
-            Log.d(TAG, "serviceCenterAddress :: " + smsMessage.getServiceCenterAddress());
-            Log.d(TAG, "time :: " + smsMessage.getTimestampMillis());
+            Log.d(TAG, "EmailBody :: " + smsMessage.emailBody);
+            Log.d(TAG, "originatingAddress :: " + smsMessage.originatingAddress);
+            Log.d(TAG, "MessageBody :: " + smsMessage.messageBody);
+            Log.d(TAG, "serviceCenterAddress :: " + smsMessage.serviceCenterAddress);
+            Log.d(TAG, "time :: " + smsMessage.timestampMillis);
 
-            val phone = smsMessage.displayOriginatingAddress
-            val message = smsMessage.messageBody
+            phone = smsMessage.displayOriginatingAddress
+            totalMsg += smsMessage.messageBody
+        }
 
-            Log.d("ASdsadsdaads", "$phone ")
-            Log.d("ASdsadsdaads", "$message ")
+        (context?.applicationContext as App).db?.let { database ->
+            CoroutineScope(Dispatchers.IO).launch {
 
-            (context?.applicationContext as App).db?.let { database ->
-                CoroutineScope(Dispatchers.IO).launch {
+                var sendingTransmissionPhoneStatus = false
+                var sendingTransmissionCharacterStatus = false
 
-                    var sendingTransmissionPhoneStatus = false
-                    var sendingTransmissionCharacterStatus = false
-
-                    withContext(CoroutineScope(Dispatchers.IO).coroutineContext) {
-                        if (database.slackDao().getPhone(phone) != null){
-                            sendingTransmissionPhoneStatus = true
-                        }
-
-                        val charactersList = database.slackDao().getIncludeCharacters()
-
-                        for(charEntity in charactersList){
-                            if (message.contains(charEntity.character.toString())){
-                                sendingTransmissionCharacterStatus = true
-                                break
-                            }
-                        }
+                withContext(CoroutineScope(Dispatchers.IO).coroutineContext) {
+                    if (database.slackDao().getPhone(phone) != null){
+                        sendingTransmissionPhoneStatus = true
                     }
 
-                    if (sendingTransmissionPhoneStatus && sendingTransmissionCharacterStatus) {
+                    val charactersList = database.slackDao().getIncludeCharacters()
+
+                    for(charEntity in charactersList){
+                        if (totalMsg.contains(charEntity.character.toString())){
+                            sendingTransmissionCharacterStatus = true
+                            break
+                        }
+                    }
+                }
+
+                if (sendingTransmissionPhoneStatus && sendingTransmissionCharacterStatus) {
                         compositeDisposable.add(
                             SlackRepository
-                                .sendMsgToSlack(RequestMsg(msg = message))
+                                .sendMsgToSlack(RequestMsg(msg = totalMsg))
                                 .subscribeOn(Schedulers.io())
                                 .observeOn(Schedulers.io())
                                 .subscribe()
                         )
-                    }
+                }
             }
         }
     }
-}
 }
